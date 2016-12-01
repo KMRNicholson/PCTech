fd2e6bb91b8cbb46136816fb8785ddb1a3cdf45c
Skarmac <kohdy.nicholson@gmail.com>
2016-11-30 22:56:53 -0400

    Styled static pages

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161130232019) do
+ActiveRecord::Schema.define(version: 20161201013404) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,6 +25,13 @@ ActiveRecord::Schema.define(version: 20161130232019) do
   create_table "resumes", force: :cascade do |t|
     t.string   "name"
     t.string   "attachment"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "name"
+    t.string   "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
