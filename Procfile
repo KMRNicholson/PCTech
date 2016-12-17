4da8878b40813daaa41cba77d67c1b5ae33b1d8e
Skarmac <kohdy.nicholson@gmail.com>
2016-12-17 18:59:59 -0400

    fixing stuff

--- Procfile
+++ Procfile
@@ -1,1 +1,7 @@
 web: bundle exec puma -C config/puma.rb
+web: node three.js
+web: node PointRenderer.js
+web: node FragmentShader.js
+web: node FirstPersonControls.js
+web: node TypedArrayUtils.js
+web: node XShader.js
--- config/application.rb
+++ config/application.rb
@@ -8,6 +8,8 @@ Bundler.require(*Rails.groups)
 
 require "active_record/railtie"
 
+config.assets.initialize_on_precompile = false
+
 module PCTech
   class Application < Rails::Application
     # Settings in config/environments/* take precedence over those specified here.
