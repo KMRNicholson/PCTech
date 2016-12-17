dc800d31fb17a77da45b4e5fcfb50bf6bfb7ea93
Skarmac <kohdy.nicholson@gmail.com>
2016-12-17 19:39:51 -0400

    fixing stuff

--- config/application.rb
+++ config/application.rb
@@ -8,12 +8,12 @@ Bundler.require(*Rails.groups)
 
 require "active_record/railtie"
 
-config.assets.initialize_on_precompile = false
-
 module PCTech
   class Application < Rails::Application
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+    config.assets.initialize_on_precompile = false
+
   end
 end
